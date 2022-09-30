56f051d925ebd14d57ae08d121dc6cba484aac6e
artursirbiladze <xjesterx404@gmail.com>
2022-09-30 11:51:50 +0300

    update organization.dto.ts

--- lib/organizations/response/organization.dto.d.ts
+++ lib/organizations/response/organization.dto.d.ts
@@ -2,4 +2,24 @@ export declare class OrganizationDto {
     responseType: string;
     id: string;
     name: string;
+    country: string;
+    restaurantAddress: string;
+    latitude: number;
+    longitude: number;
+    useUaeAddressingSystem: boolean;
+    version: string;
+    currencyIsoName: string;
+    currencyMinimumDenomination: number;
+    countryPhoneCode: string;
+    marketingSourceRequiredInDelivery: boolean;
+    defaultDeliveryCityId: string;
+    deliveryCityIds: Array<string>;
+    deliveryServiceType: string;
+    defaultCallCenterPaymentTypeId: string;
+    orderItemCommentEnabled: boolean;
+    inn: string;
+    addressFormatType: string;
+    isConfirmationEnabled: boolean;
+    confirmAllowedIntervalInMinutes: number;
+    isCloud: boolean;
 }
--- src/organizations/response/organization.dto.ts
+++ src/organizations/response/organization.dto.ts
@@ -4,4 +4,44 @@ export declare class OrganizationDto {
   id: string;
 
   name: string;
+
+  country: string;
+
+  restaurantAddress: string;
+
+  latitude: number;
+
+  longitude: number;
+
+  useUaeAddressingSystem: boolean;
+
+  version: string;
+
+  currencyIsoName: string;
+
+  currencyMinimumDenomination: number;
+
+  countryPhoneCode: string;
+
+  marketingSourceRequiredInDelivery: boolean;
+
+  defaultDeliveryCityId: string;
+
+  deliveryCityIds: Array<string>;
+
+  deliveryServiceType: string;
+
+  defaultCallCenterPaymentTypeId: string;
+
+  orderItemCommentEnabled: boolean;
+
+  inn: string;
+
+  addressFormatType: string;
+
+  isConfirmationEnabled: boolean;
+
+  confirmAllowedIntervalInMinutes: number;
+
+  isCloud: boolean;
 }
