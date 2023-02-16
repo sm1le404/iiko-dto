900e478be470b8dcb669ff6f048bab4b8c5e2fa6
artursirbiladze <xjesterx404@gmail.com>
2023-02-16 15:13:00 +0300

    update customer

--- lib/customer/response/response-customer.dto.d.ts
+++ lib/customer/response/response-customer.dto.d.ts
@@ -1,3 +1,17 @@
 export declare class ResponseCustomerDto {
     id: string;
+    phone: string;
+    cardTrack: string;
+    cardNumber: string;
+    name: string;
+    middleName: string;
+    surName: string;
+    birthday: Date;
+    email: string;
+    sex: number;
+    consentStatus: number;
+    shouldReceivePromoActionsInfo: boolean;
+    referrerId: string;
+    userData: string;
+    organizationId: string;
 }
--- src/customer/response/response-customer.dto.ts
+++ src/customer/response/response-customer.dto.ts
@@ -1,3 +1,31 @@
 export class ResponseCustomerDto {
   id: string;
+
+  phone: string;
+
+  cardTrack: string;
+
+  cardNumber: string;
+
+  name: string;
+
+  middleName: string;
+
+  surName: string;
+
+  birthday: Date;
+
+  email: string;
+
+  sex: number;
+
+  consentStatus: number;
+
+  shouldReceivePromoActionsInfo: boolean;
+
+  referrerId: string;
+
+  userData: string;
+
+  organizationId: string;
 }
